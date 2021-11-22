5838c3dfd5aedd19ff85aacc94d1c68706c7530b
Nikuu <oskarradwan@wp.pl>
2021-11-22 18:00:10 +0100

    18:00

--- snake.js
+++ snake.js
@@ -9,15 +9,49 @@ var snake = [
 ]
 
 
-
-function drawsnake()
-{
-    planszaCtx.fillStyle='green';
-    planszaCtx.fillRect(snakePart.x, snakePart.y, 10, 10);
-}
-
-
 function drawSnake()
 {
-    snake.forEach(drawsnake);
+    snake.forEach(drawSnakeElement);
 }
+
+
+
+function clear()
+{
+    planszaCtx.fillStyle = "black";
+    planszaCtx.fillRect(0, 0, plansza.width, plansza.height);
+}
+
+
+
+function drawSnakeElement(snakeElement)
+{
+    planszaCtx.fillStyle = "lightgreen";
+    planszaCtx.fillRect(snakeElement.x, snakeElement.y, 10, 10);
+    planszaCtx.strokestyle = "black";
+    planszaCtx.strokeRect(snakeElement.x, snakeElement.y, 10, 10);
+}
+
+var dx=10;
+var dy=0;
+
+function move()
+{
+    var head = {x: snake[0].x + dx, y: snake[0].y + dy};
+    snake.unshift(head);
+    snake.pop();
+}
+
+
+function main()
+{
+    setInterval(function step(){
+        clear();
+        move();
+        drawSnake();
+    }, 1000);
+    
+}
+
+
+main();
