8974614edd5dd52f67af08dfedd78495f6ab09e3
Nikuu <oskarradwan@wp.pl>
2022-01-18 21:49:10 +0100

    1.4.4 modalbox fixed

--- snake.js
+++ snake.js
@@ -433,6 +433,6 @@ else
     document.getElementById("playmobile").innerHTML="";
 }
 
-document.getElementById("version").innerHTML="v1.4.3";
+document.getElementById("version").innerHTML="v1.4.4";
 
 //main();
